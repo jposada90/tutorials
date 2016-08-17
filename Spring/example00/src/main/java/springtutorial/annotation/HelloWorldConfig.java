ab0c08e2c84a012107d2e05232231f227654aabc
Jesús Posada <jesus.posadaayala@altran.com>
2016-08-17 09:57:53 +0200

    Springtutorial: annotation example, inner beans example modified with annotations

---
+++ Spring/example00/src/main/java/springtutorial/annotation/HelloWorldConfig.java
@@ -0,0 +1,12 @@
+package springtutorial.annotation;
+
+import org.springframework.context.annotation.*;
+
+@Configuration
+public class HelloWorldConfig {
+
+   @Bean 
+   public HelloWorld helloWorld(){
+      return new HelloWorld();
+   }
+}
